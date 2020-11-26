b52a07a1a72c7351c8147f94e5481de6cd94ee57
ashish936 <bishtashish936@gmail.com>
2020-11-26 12:01:35 +0530

    Basic design ready
    just working on the basic design and improving

---
+++ script.js
@@ -0,0 +1,9 @@
+//funtion for making the nav bar responsive
+function navBar() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
